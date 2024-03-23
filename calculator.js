e90d96a2f5baf0dd4653cd6ad39428ddebfabb5f
AngelCia <131485313+AngelCia@users.noreply.github.com>
2024-03-23 01:53:18 -0400

    Calculator script added, new font added

--- calculator.js
+++ calculator.js
@@ -1,4 +1,10 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b){
+    a = parseInt(a);
+    b = parseInt(b);
     return a + b;
 }
 
@@ -15,7 +21,6 @@ function divide(a, b){
 }
 
 function operate(a, operator, b){
-
     switch (operator) {
         case "+": 
             return add(a, b);
@@ -32,6 +37,46 @@ function operate(a, operator, b){
     }
 }
 
-let num1;
-let num2;
-let operator;
+function clearOut(){
+    num1 = "";
+    num2 = "";
+    operator = "";
+    document.getElementById("display").innerHTML = num1;
+
+}
+
+function displayNums(idName, className){
+    if(!num1 && className == "numbers"){
+        num1 = idName;
+        document.getElementById("display").innerHTML = num1;
+
+    }else if((num1) && className == "numbers" && !operator){
+        num1 = "" + num1 + idName;
+        document.getElementById("display").innerHTML = num1;
+
+    }else if(num1 && className == "operators" && !operator){
+        operator = idName;
+        document.getElementById("display").innerHTML = operator;
+
+    }else if(num1 && operator && className == "numbers" && !num2){
+        num2 = idName;
+        document.getElementById("display").innerHTML = num2;
+
+    }else if(num1 && operator && className == "numbers" && num2){
+        num2 = "" + num2 + idName;
+        document.getElementById("display").innerHTML = num2;
+
+    } else if(num1 && operator && num2 && className == "operators"){
+        num1 = operate(num1, operator, num2);
+        document.getElementById("display").innerHTML = num1;
+        operator = idName;
+        num2 = "";
+        
+    }else if(num1 && operator && num2 && idName == "="){
+        num1 = operate(num1, operator, num2);
+        document.getElementById("display").innerHTML = num1;
+        operator = "";
+        num2 = "";
+    }
+}
+
