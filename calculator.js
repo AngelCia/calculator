3f245b379f4a4e6dd0e15c7af2033f5335b1fdd7
AngelCia <131485313+AngelCia@users.noreply.github.com>
2024-03-22 22:40:02 -0400

    Add html and css to calculator

--- calculator.js
+++ calculator.js
@@ -1,15 +1,37 @@
-export function add(a, b){
+function add(a, b){
     return a + b;
 }
 
-export function subtract(a, b){
+function subtract(a, b){
     return a - b;
 }
 
-export function multiply(a, b){
+function multiply(a, b){
     return a * b;
 }
 
-export function divide(a, b){
+function divide(a, b){
     return a / b;
 }
+
+function operate(a, operator, b){
+
+    switch (operator) {
+        case "+": 
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;            
+    }
+}
+
+let num1;
+let num2;
+let operator;
